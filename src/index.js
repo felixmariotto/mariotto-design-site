d6b07eff37ed4c83c93c2f83024ff360eafe1dc9
Felix Mariotto <felixmariotto@gmail.com>
2022-04-14 15:53:05 +0200

    create i18n system

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './index.css';
 import Component from './components/Component.js';
+import './i18n/init.js';
 
 const title = Component({ tagName:"H1", i18n: "title" });
 const subtitle = Component({ tagName:"H2", i18n: "subtitle" });
