e16d0e0a15d5e9b66241e7dd486317500a9a1da1
Felix Mariotto <felixmariotto@gmail.com>
2022-04-15 10:47:26 +0200

    improve i18n

--- src/i18n/init.js
+++ src/i18n/init.js
@@ -2,45 +2,37 @@
 import frTranscript from './lang/fr.json';
 import enTranscript from './lang/en.json';
 import itTranscript from './lang/it.json';
-let transcript = enTranscript;
 
-// The locale our app first shows
+//
+
 const defaultLocale = "en";
-const supportedLocales = [ "en", "fr", "it" ];
 let locale = defaultLocale;
+
+const transcripts = {
+	en: enTranscript,
+	fr: frTranscript,
+	it: itTranscript
+};
+
+const defaultTranscript = transcripts[ defaultLocale ];
+let transcript = transcripts[ locale ];
+
+//
 
 window.addEventListener( 'load', () => {
 
 	// Retrieve user-preferred locales from the browser
 	const browserLocales = navigator.languages.map( locale => locale.split( "-" )[ 0 ] );
 
-	const intersectLocales = browserLocales.filter( e => supportedLocales.includes( e ) );
+	const intersectLocales = browserLocales.filter( e => Object.keys( transcripts ).includes( e ) );
 
 	locale = intersectLocales[0] || defaultLocale;
 
-	updateTranscript();
+	transcript = transcripts[ locale ];
 
 	translatePage();
 
 } );
-
-// update the default updateTranscript accroding to the current locale.
-function updateTranscript() {
-	switch ( locale ) {
-		case 'en':
-			transcript = enTranscript;
-			break
-		case 'fr':
-			transcript = frTranscript;
-			break
-		case 'it':
-			transcript = itTranscript;
-			break
-		default:
-			transcript = enTranscript;
-			break
-	}
-}
 
 // Replace the inner text of each element that has a
 // i18n-key attribute with the translation corresponding
@@ -56,6 +48,6 @@ function translatePage() {
 // corresponding to the element's i18n-key
 function translateElement( element ) {
   const key = element.getAttribute( "i18n-key" );
-  const text = transcript[ key ];
+  const text = transcript[ key ] || defaultTranscript[ key ];
   element.innerText = text;
 }
