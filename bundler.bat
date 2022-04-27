0314d6e84a8aadd3dca69d9b31df39d58a0a3e61
Felix Mariotto <felixmariotto@gmail.com>
2022-04-27 18:01:00 +0200

    improve batch script by cleanup up the dist folder and copying the assets from another source

--- bundler.bat
+++ bundler.bat
@@ -1,4 +1,11 @@
+:: recreate an empty dist folder
+rmdir /s/q dist
+mkdir dist
+
+:: copy the assets folder in the dist folder
+Xcopy "src/assets" "dist/assets" /E /I
+
 :: special pages
-call ejs ./templates/template.ejs -f ./pages_data/index-data.json -o ./dist/index.html
-call ejs ./templates/template.ejs -f ./pages_data/404-data.json -o ./dist/404.html
+call ejs ./src/template.ejs -f ./src/pages_data/index-data.json -o ./dist/index.html
+call ejs ./src/template.ejs -f ./src/pages_data/404-data.json -o ./dist/404.html
 :: articles
