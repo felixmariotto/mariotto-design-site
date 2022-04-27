02ddb80bb8050960c91a827b2e694499df95af32
Felix Mariotto <felixmariotto@gmail.com>
2022-04-27 16:44:55 +0200

    setup templating

---
+++ script.bat
@@ -0,0 +1,5 @@
+:: special pages
+
+:: articles
+call ejs ./templates/template_file.ejs -f ./pages_data/data_file.json -o ./dist/article1.html
+call ejs ./templates/template_file.ejs -f ./pages_data/data_file.json -o ./dist/article2.html
