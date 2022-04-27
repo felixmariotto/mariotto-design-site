e815801c73c09f0dce6528a2b326ccfe4e4d9150
Felix Mariotto <felixmariotto@gmail.com>
2022-04-27 21:47:48 +0200

    add services, about and contact pages

--- bundler.bat
+++ bundler.bat
@@ -8,4 +8,7 @@ Xcopy "src/assets" "dist/assets" /E /I
 :: special pages
 call ejs ./src/template.ejs -f ./src/pages_data/index-data.json -o ./dist/index.html
 call ejs ./src/template.ejs -f ./src/pages_data/404-data.json -o ./dist/404.html
+call ejs ./src/template.ejs -f ./src/pages_data/services-data.json -o ./dist/services.html
+call ejs ./src/template.ejs -f ./src/pages_data/about-data.json -o ./dist/about.html
+call ejs ./src/template.ejs -f ./src/pages_data/contact-data.json -o ./dist/contact.html
 :: articles
