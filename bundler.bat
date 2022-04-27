1e5c3c19dbd24c46027732a24cc576d8ffec7664
Felix Mariotto <felixmariotto@gmail.com>
2022-04-27 17:38:00 +0200

    create 404 page with templating

---
+++ bundler.bat
@@ -0,0 +1,4 @@
+:: special pages
+call ejs ./templates/template.ejs -f ./pages_data/index-data.json -o ./dist/index.html
+call ejs ./templates/template.ejs -f ./pages_data/404-data.json -o ./dist/404.html
+:: articles
