9fb325e424c0a241ded37d128f27a137ca3b94ea
Felix Mariotto <felixmariotto@gmail.com>
2022-04-28 11:14:26 +0200

    update the bundling script to add articles data to the pages data because EJS is limited

--- bundler.bat
+++ bundler.bat
@@ -6,6 +6,12 @@ mkdir dist\articles
 :: copy the assets folder in the dist folder
 Xcopy "src/assets" "dist/assets" /E /I
 
+:: add the content of src/pages_data/articles-list.json in the index and articles pages files.
+:: we need to do this because it's not possible to pass several JSON to EJS bellow,
+:: so the articles data that is needed in several pages must be added manually to
+:: the single JSON file that we give to EJS.
+call node jsonParser.js
+
 :: special pages
 call ejs ./src/template.ejs -f ./src/pages_data/index-data.json -o ./dist/index.html
 call ejs ./src/template.ejs -f ./src/pages_data/404-data.json -o ./dist/404.html
