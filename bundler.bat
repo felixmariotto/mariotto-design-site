e0ff5e15efba5299c61da04e586f5665f3348847
Felix Mariotto <felixmariotto@gmail.com>
2022-04-28 10:00:40 +0200

    create articles pages

--- bundler.bat
+++ bundler.bat
@@ -1,6 +1,7 @@
 :: recreate an empty dist folder
 rmdir /s/q dist
 mkdir dist
+mkdir dist\articles
 
 :: copy the assets folder in the dist folder
 Xcopy "src/assets" "dist/assets" /E /I
@@ -11,4 +12,8 @@ call ejs ./src/template.ejs -f ./src/pages_data/404-data.json -o ./dist/404.html
 call ejs ./src/template.ejs -f ./src/pages_data/services-data.json -o ./dist/services.html
 call ejs ./src/template.ejs -f ./src/pages_data/about-data.json -o ./dist/about.html
 call ejs ./src/template.ejs -f ./src/pages_data/contact-data.json -o ./dist/contact.html
+call ejs ./src/template.ejs -f ./src/pages_data/articles-data.json -o ./dist/articles/index.html
 :: articles
+call ejs ./src/template.ejs -f ./src/pages_data/articles/collab-lagarde.json -o ./dist/articles/collab-lagarde.html
+call ejs ./src/template.ejs -f ./src/pages_data/articles/rh-aw-pav.json -o ./dist/articles/rh-aw-pav.html
+call ejs ./src/template.ejs -f ./src/pages_data/articles/gemviewr.json -o ./dist/articles/gemviewr.html
